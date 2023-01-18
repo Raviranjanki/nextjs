80d0ad107785c614b04aeb10a845c0ad80b246cc
Ravi Ranjan Kumar <ravi.ranjan@ucodesoft.com>
2023-01-18 21:47:24 +0530

    setup commit

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -122,7 +122,7 @@ const login = () => {
               Login
             </button>
             <p className="text-sm font-semibold mt-2 pt-1 mb-0">
-              Don't have an account?
+              Dont have an account?
               <Link
                 href="/signup"
                 className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
