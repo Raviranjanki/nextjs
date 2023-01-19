9a50cb2c292cc9d102d3b76ed20f3e4f72870a2b
Ravi Ranjan Kumar <ravi.ranjan@ucodesoft.com>
2023-01-19 21:49:07 +0530

    added some static images in public folder

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 const signup = () => {
@@ -6,10 +7,13 @@ const signup = () => {
       <div className="px-6 h-full text-gray-800">
         <div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
           <div className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0">
-            <img
+            {/* <img
               src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
               className="w-full"
               alt="Sample image"
+            /> */}
+            <Image
+              src={'/img/logo/img1.jpeg'}
             />
           </div>
           <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
