56e42a50de70d730dcb27fde6073eb531dba91e3
Ravi Ranjan Kumar <ravi.ranjan@ucodesoft.com>
2023-03-31 22:04:14 +0530

    wefwef

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -21,7 +21,6 @@ export async function POST(request) {
     from: '"Fred Foo 👻" <foo@example.com>', // sender address
     to: "bar@example.com, baz@example.com", // list of receivers
     subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
     html: "<b>Hello world?</b>", // html body
   });
 
