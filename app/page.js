95beed357ffd2de608bdea726449b47712dbff3d
Ravi Ranjan Kumar <ravi.ranjan@ucodesoft.com>
2023-03-31 21:36:17 +0530

    wefwef

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+'use client'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
@@ -5,9 +6,16 @@ import styles from './page.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const handle = () => {
+    fetch('/api/hello', {
+      method: 'POST'
+    })
+  }
+
   return (
     <main className={styles.main}>
-      
+      <button onClick={handle}>Fetch</button>
     </main>
   )
 }
