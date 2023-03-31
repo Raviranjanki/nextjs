7082c574c5156370e1b1211dc537b6530cdeb26a
Ravi Ranjan Kumar <ravi.ranjan@ucodesoft.com>
2023-03-31 22:26:03 +0530

    wefwef

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,4 +1,4 @@
-import { scheduleJob } from "node-schedule"
+import { cancelJob, scheduleJob } from "node-schedule"
 import nodemailer from 'nodemailer'
 
 export async function GET(request) {
@@ -8,30 +8,32 @@ export async function GET(request) {
 export async function POST(request) {
 
   // create reusable transporter object using the default SMTP transport
+  console.log('qwdknjwqd');
   let transporter = nodemailer.createTransport({
-    host: "gmail",
+    service: "gmail",
     auth: {
       user: 'ravi.ranjan@ucodesoft.com', // generated ethereal user
       pass: '@UcodeRavi$123%', // generated ethereal password
     },
   });
 
-  let info = await transporter.sendMail({
-    from: 'mr.ranjan.officials@gmail.com', // sender address
-    to: "mr.ranjan.officials@gmail.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    html: "<b>Hello world?</b>", // html body
-  });
-
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-
-  scheduleJob('job', '*/2 * * * * *',()=>{
+  
+  scheduleJob('job', '*/2 * * * * *', async()=>{
     console.log('job running')
-    
+    let info = transporter.sendMail({
+      from: 'mr.ranjan.officials@gmail.com', // sender address
+      to: "mr.ranjan.officials@gmail.com", // list of receivers
+      subject: "Hello ✔", // Subject line
+      html: "<b>Hello world?</b>", // html body
+    });
+  
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  
+    cancelJob('job')
   })
   return new Response('Hello, Next.js!')
 }
